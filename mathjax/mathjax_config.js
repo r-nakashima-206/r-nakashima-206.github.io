b43096140626bbb292170787cf5ae32a6eecf96d
nakashima <r.nakashima.a10@m.kyushu-u.ac.jp>
2020-05-03 23:09:57 +0900

    headを加筆

---
+++ mathjax/mathjax_config.js
@@ -0,0 +1,16 @@
+window.MathJax = {
+
+tex: {
+    /* 本文中の数式 */
+    inlineMath: [
+        ['$', '$']
+    ],
+    macros: {
+        bm: ["{\\boldsymbol #1}", 1]
+    }
+},
+svg: {
+    fontCache: 'global'
+}
+    
+};
