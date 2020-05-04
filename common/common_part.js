196bd8e8f2b27b1989b0f2ad24b30d9a6ed241f5
nakashima <r.nakashima.a10@m.kyushu-u.ac.jp>
2020-05-04 16:31:19 +0900

    共通パーツを分割

---
+++ common/common_part.js
@@ -0,0 +1,4 @@
+$(function(){
+    $("#common_head").load("./common/include/common_head.html");
+    $("#menu").load("./common/include/menu.html");
+});
---
+++ common/mathjax/mathjax_config.js
@@ -0,0 +1,31 @@
+window.MathJax = {
+
+tex: {
+    // 本文中の数式 //
+    inlineMath: [
+        ['$', '$']
+    ],
+    // ディスプレイ数式モード //
+    displayMath: [
+        ['\\[', '\\]']
+    ],
+    // マクロ //
+    macros: {
+        bm: ["{\\boldsymbol #1}", 1],
+        
+        uO: "{\\mathrm{O}}",
+        ud: "{\\mathrm{d}}",
+        uD: "{\\mathrm{D}}",
+        ue: "{\\mathrm{e}}",
+        ui: "{\\mathrm{i}}",
+        ul: "{\\ell}",
+        upi: "{\\Large\\unicode[0.5,0.05][times]{x3c0}}",
+        upartial: "{\\Large\\unicode[0.5,0.05][times]{x2202}}"
+    }
+},
+
+svg: {
+    fontCache: 'global'
+}
+
+};
