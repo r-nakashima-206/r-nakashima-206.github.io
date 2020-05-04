7500ece6abac32d622acb3b594d28c5fcd6e9670
nakashima <r.nakashima.a10@m.kyushu-u.ac.jp>
2020-05-04 11:51:31 +0900

    upartialとupiのマクロ作成

--- mathjax/mathjax_config.js
+++ mathjax/mathjax_config.js
@@ -8,7 +8,20 @@ tex: {
     // ディスプレイ数式モード //
     displayMath: [
         ['\\[', '\\]']
-    ]
+    ],
+    // マクロ //
+    macros: {
+        bm: ["{\\boldsymbol #1}", 1],
+        
+        uO: "{\\mathrm{O}}",
+        ud: "{\\mathrm{d}}",
+        uD: "{\\mathrm{D}}",
+        ue: "{\\mathrm{e}}",
+        ui: "{\\mathrm{i}}",
+        ul: "{\\ell}",
+        upi: "{\\large\\unicode[0.5,0.05][times]{x3c0}}",
+        upartial: "{\\large\\unicode[0.5,0.05][times]{x2202}}"
+    }
 },
 
 svg: {
