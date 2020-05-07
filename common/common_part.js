a0bb548c9a7a826eaba003c565a2943a7cff3f98
nakashima <r.nakashima.a10@m.kyushu-u.ac.jp>
2020-05-07 12:19:52 +0900

    common_partを編集

--- common/common_part.js
+++ common/common_part.js
@@ -4,7 +4,7 @@ $(function(){
     $("#header").load("./common/include/header.html");
     $("#footer").load("./common/include/footer.html");
 // in r-nakashima-geophysics.github.io/hitasura_linear_analysis/index.html
-    $("#common_head").load("../common/include/common_head.html");
+    $("#common_head-hla").load("../common/include/common_head.html");
     $("#header-hla").load("../common/include/header-hla.html");
     $("#footer-hla").load("../common/include/footer-hla.html");
 });
